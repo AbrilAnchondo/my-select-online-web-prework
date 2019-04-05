4d204d09927032d1e037da3ad3ee72d1a092d6c6
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-04-05 23:10:41 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -3,7 +3,8 @@ def my_select(collection)
 new_collection = []
 i = 0 
 while collection.length < i 
-yield(array[i])
+if yield(array[i]) == true 
+  new_collection.push(yield(array[i]))
 i += 1 
 end 
 new_collection
