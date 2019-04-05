8e4da8fdab95b2b0ee948126e871cc0b215f8f5e
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-04-05 23:15:17 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,14 +1,14 @@
 def my_select(collection)
  # your code here!
 new_collection = []
-i = 0 
-while collection.length < i 
-if yield(array[i]) #if true
-  new_collection.push(array[i])
-end
-  
-i += 1 
-end 
-new_collection
+  i = 0 
+  while collection.length < i 
+    if yield(array[i]) #if true
+      new_collection.push(array[i])
+    end
+  i += 1 
+  end 
+  new_collection
 end
 
+my_select()
